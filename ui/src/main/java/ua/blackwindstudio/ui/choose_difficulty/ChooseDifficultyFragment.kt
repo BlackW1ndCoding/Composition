58e9014a2711ba8c43f728f1162e34d3d271c40d
Andriy Tarasenko <black.wind.coding@gmail.com>
2022-08-25 20:02:54 +0300

    added choose difficulty fragment layout and class

---
+++ ui/src/main/java/ua/blackwindstudio/ui/choose_difficulty/ChooseDifficultyFragment.kt
@@ -0,0 +1,7 @@
+package ua.blackwindstudio.ui.choose_difficulty
+
+import androidx.fragment.app.Fragment
+import ua.blackwindstudio.ui.R
+
+class ChooseDifficultyFragment: Fragment(R.layout.fragment_choose_difficulty) {
+}
