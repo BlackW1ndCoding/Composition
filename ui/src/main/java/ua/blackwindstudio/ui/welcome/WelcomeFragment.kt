b30af3e30218c132404fb452aec90effb7dcd557
Andriy Tarasenko <black.wind.coding@gmail.com>
2022-08-26 17:12:01 +0300

    added view binding to ui module

--- ui/src/main/java/ua/blackwindstudio/ui/welcome/WelcomeFragment.kt
+++ ui/src/main/java/ua/blackwindstudio/ui/welcome/WelcomeFragment.kt
@@ -1,7 +1,17 @@
 package ua.blackwindstudio.ui.welcome
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import ua.blackwindstudio.ui.R
+import ua.blackwindstudio.ui.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment: Fragment(R.layout.fragment_welcome) {
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentWelcomeBinding.bind(view)
+    }
 }
